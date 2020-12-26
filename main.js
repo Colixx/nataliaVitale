16ad15a8a233d5d2e440a45367ebb85d53716f6e
Colixx <nataliavitale23@gmail.com>
2020-12-26 17:41:33 -0300

    animaciones

---
+++ main.js
@@ -0,0 +1,18 @@
+var cine = document.getElementById("remote1");
+function chanfge(image){
+    cine.src=image.src;
+}
+var ana = document.getElementById("ana");
+function change(image){
+    ana.src=image.src;
+}
+
+var chile = document.getElementById("chile");
+function changef(image){
+    chile.src=image.src;
+}
+
+var fuga = document.getElementById("fuga");
+function changes(image){
+    fuga.src=image.src;
+}
